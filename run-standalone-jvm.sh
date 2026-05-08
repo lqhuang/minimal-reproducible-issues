f108b1b9e6b0bcbceff0ee1beea0efd235055aea
Lanqing Huang <lqhuang@outlook.com>
2026-05-08 17:13:51 +0800

    Add run-standalone-jvm.sh

---
+++ run-standalone-jvm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+MACHINE_NAME="demo"
+CURR_REPO=$(dirname "$(realpath "$0")")
+
+SCALA_CLI=scala
+
+if [[ ! -x $(command -v "${SCALA_CLI}") ]]; then
+  echo "Error: '${SCALA_CLI}' command not found. Please install Scala CLI or update SCALA_CLI." >&2
+  exit 1
+fi
+
+TESTS=(
+  Loops1Test
+  Loops2Test
+  Loops3Test
+  Loops4Test
+)
+
+for test_script in "${TESTS[@]}"; do
+  echo "Running ${test_script}..."
+
+  log_dir="${CURR_REPO}/logs/${MACHINE_NAME}/standalone/jvm/${test_script}"
+  mkdir -p "${log_dir}"
+
+  for i in {0..49}; do
+    scala run --platform jvm --scala-version 3.8.3 \
+          "${CURR_REPO}/standalone/${test_script}.scala" 2>&1 \
+      | tee "${log_dir}/$(printf %02d "${i}").log"
+  done
+
+  echo "Finished ${test_script} for libc-stdatomic"
+done
+popd
+
